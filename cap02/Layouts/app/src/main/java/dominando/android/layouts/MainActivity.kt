fb7ce0686ea2e968f4bd79cceb5be7725dac4b53
Jose Pereira <josebatistapereira@gmail.com>
2019-03-26 23:41:30 -0300

    Exemplo ScrollView

--- cap02/Layouts/app/src/main/java/dominando/android/layouts/MainActivity.kt
+++ cap02/Layouts/app/src/main/java/dominando/android/layouts/MainActivity.kt
@@ -1,12 +1,12 @@
 package dominando.android.layouts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.ex_scroll)
     }
 }
