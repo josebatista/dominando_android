f8ee71b9e0a04b8108b4f012a45cca42a4aafb2b
Jose Pereira <josebatistapereira@gmail.com>
2019-03-28 23:07:39 -0300

    Obtendo dados de outra Activity

---
+++ cap03/ActivityResult/app/src/main/java/dominando/android/activityresult/StatesListActivity.kt
@@ -0,0 +1,46 @@
+package dominando.android.activityresult
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.AbsListView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+class StatesListActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_STATE = "estado"
+        const val EXTRA_RESULT = "result"
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val listView = ListView(this)
+        setContentView(listView)
+
+        val stateList = resources.getStringArray(R.array.states)
+        val listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, stateList)
+
+        listView.adapter = listAdapter
+        listView.choiceMode = AbsListView.CHOICE_MODE_SINGLE
+
+        val state = intent.getStringExtra(EXTRA_STATE)
+        if (state != null) {
+            val position = stateList.indexOf(state)
+            listView.setItemChecked(position, true)
+        }
+
+        listView.setOnItemClickListener { parent, _, position, _ ->
+            val result = parent.getItemAtPosition(position) as String // ou "val result = stateList[position]"
+            val it = Intent()
+            it.putExtra(EXTRA_RESULT, result)
+
+            setResult(Activity.RESULT_OK, it)
+            finish()
+        }
+    }
+}
