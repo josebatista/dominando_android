0865eebb0f805829664c670b5e31d3e1179cf65a
Jose Pereira <josebatistapereira@gmail.com>
2019-03-30 11:40:19 -0300

    Funcionalidades básicas implementadas

--- cap03/Intents/app/src/main/java/dominando/android/intents/MainActivity.kt
+++ cap03/Intents/app/src/main/java/dominando/android/intents/MainActivity.kt
@@ -1,12 +1,112 @@
 package dominando.android.intents
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.SearchManager
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.provider.Settings
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val listView = ListView(this)
+        setContentView(listView)
+
+        val adapter = ArrayAdapter(
+            this, android.R.layout.simple_list_item_1,
+            resources.getStringArray(R.array.intent_actions)
+        )
+
+        listView.adapter = adapter
+        listView.setOnItemClickListener { _, _, position, _ ->
+            openIntentAtPosition(position)
+        }
+    }
+
+    private fun openIntentAtPosition(position: Int) {
+
+        val uri: Uri?
+        val intent: Intent?
+
+        when (position) {
+            0 -> { //Abrindo URL
+                uri = Uri.parse("http://www.terra.com")
+                intent = Intent(Intent.ACTION_VIEW, uri)
+                openIntent(intent)
+            }
+            1 -> { //Realiza uma chamada
+                uri = Uri.parse("tel:999887766")
+                intent = Intent(Intent.ACTION_DIAL, uri)
+                openIntent(intent)
+            }
+            2 -> { //Pesquisa uma posicao no mapa
+                //Seu emulador/aparelho deve ter o Google Maps
+                uri = Uri.parse("geo:0,0?q=Rua+Amelia,Recife")
+                intent = Intent(Intent.ACTION_VIEW, uri)
+                openIntent(intent)
+            }
+            3 -> { //Abrindo o editro de SMS
+                uri = Uri.parse("sms:12345")
+                intent = Intent(Intent.ACTION_VIEW, uri)
+                    .putExtra("sms_body", "Corpo do SMS")
+                openIntent(intent)
+            }
+            4 -> { //Compartilhar
+                intent = Intent()
+                    .setAction(Intent.ACTION_SEND)
+                    .putExtra(Intent.EXTRA_TEXT, "Compartilhando via intent")
+                    .setType("text/plain")
+                openIntent(intent)
+            }
+            5 -> { //Alarme
+                intent = Intent(AlarmClock.ACTION_SET_ALARM)
+                    .putExtra(AlarmClock.EXTRA_MESSAGE, "Estudar Android")
+                    .putExtra(AlarmClock.EXTRA_HOUR, 19)
+                    .putExtra(AlarmClock.EXTRA_MINUTES, 0)
+                    .putExtra(AlarmClock.EXTRA_SKIP_UI, true)
+                    .putExtra(
+                        AlarmClock.EXTRA_DAYS, arrayListOf(
+                            Calendar.MONDAY,
+                            Calendar.WEDNESDAY,
+                            Calendar.FRIDAY
+                        )
+                    )
+                openIntent(intent)
+            }
+            6 -> { //Busca na web
+                intent = Intent(Intent.ACTION_SEARCH)
+                    .putExtra(SearchManager.QUERY, "Novatec")
+                openIntent(intent)
+            }
+            7 -> { //Configuracoes do aparelho
+                intent = Intent(Settings.ACTION_SETTINGS)
+                openIntent(intent)
+            }
+            8 -> { //Acao customizada 1
+                intent = Intent("dominando.android.CUSTOM_ACTION")
+                openIntent(intent)
+            }
+            9 -> { //Acao customizada 2
+                intent = Intent("produto://Notebook/Slim")
+                openIntent(intent)
+            }
+            else -> finish()
+        }
+    }
+
+    private fun openIntent(intent: Intent) {
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, R.string.error_intent, Toast.LENGTH_SHORT).show()
+        }
     }
 }
