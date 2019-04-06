a252e67afe58939ed20aa19665b4341bd2e41e5a
Jose Pereira <josebatistapereira@gmail.com>
2019-04-05 22:38:23 -0300

    Detectando o clique em um item da ListView

--- cap05/Adapter/app/src/main/java/dominando/android/adapter/MainActivity.kt
+++ cap05/Adapter/app/src/main/java/dominando/android/adapter/MainActivity.kt
@@ -2,28 +2,13 @@ package dominando.android.adapter
 
 import android.os.Bundle
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     //0=VW;1=GM;2=Fiat;3=Ford
     private val vehicles = mutableListOf(
-        Vehicle("Onix", 2018, 1, true, true),
-        Vehicle("Uno", 2007, 2, true, false),
-        Vehicle("Del Rey", 1988, 3, false, true),
-        Vehicle("Gol", 2014, 0, true, true),
-        Vehicle("Onix", 2018, 1, true, true),
-        Vehicle("Uno", 2007, 2, true, false),
-        Vehicle("Del Rey", 1988, 3, false, true),
-        Vehicle("Gol", 2014, 0, true, true),
-        Vehicle("Onix", 2018, 1, true, true),
-        Vehicle("Uno", 2007, 2, true, false),
-        Vehicle("Del Rey", 1988, 3, false, true),
-        Vehicle("Gol", 2014, 0, true, true),
-        Vehicle("Onix", 2018, 1, true, true),
-        Vehicle("Uno", 2007, 2, true, false),
-        Vehicle("Del Rey", 1988, 3, false, true),
-        Vehicle("Gol", 2014, 0, true, true),
         Vehicle("Onix", 2018, 1, true, true),
         Vehicle("Uno", 2007, 2, true, false),
         Vehicle("Del Rey", 1988, 3, false, true),
@@ -37,5 +22,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(listView)
         val adapter = VehicleAdapter(this, vehicles)
         listView.adapter = adapter
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val (model, year) = vehicles[position]
+            Toast.makeText(this, "$model $year", Toast.LENGTH_SHORT).show()
+        }
     }
 }
