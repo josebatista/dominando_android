c59f8abb87a7a6ae838ddf0aea70207cfb616e8e
Jose Pereira <josebatistapereira@gmail.com>
2019-04-05 22:53:53 -0300

    Adicionando cabecalho e rodape na listview

--- cap05/Adapter/app/src/main/java/dominando/android/adapter/MainActivity.kt
+++ cap05/Adapter/app/src/main/java/dominando/android/adapter/MainActivity.kt
@@ -1,7 +1,10 @@
 package dominando.android.adapter
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         Vehicle("Gol", 2014, 0, true, true)
     )
 
+    private lateinit var txtFooter: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,9 +28,31 @@ class MainActivity : AppCompatActivity() {
         val adapter = VehicleAdapter(this, vehicles)
         listView.adapter = adapter
 
-        listView.setOnItemClickListener { parent, view, position, id ->
-            val (model, year) = vehicles[position]
-            Toast.makeText(this, "$model $year", Toast.LENGTH_SHORT).show()
+        initHeaderAndFooter(listView, adapter)
+
+        listView.setOnItemClickListener { parent, _, position, _ ->
+            val vehicle = parent.getItemAtPosition(position) as? Vehicle
+            if (vehicle != null) {
+                val (model, year) = vehicle
+                Toast.makeText(this, "$model $year", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun initHeaderAndFooter(listView: ListView, adapter: VehicleAdapter) {
+        val padding = 8
+        val txtHeader = TextView(this)
+        txtHeader.setBackgroundColor(Color.GRAY)
+        txtHeader.setTextColor(Color.WHITE)
+        txtHeader.setText(R.string.header_text)
+        txtHeader.setPadding(padding, padding, 0, padding)
+        listView.addHeaderView(txtHeader)
+
+        txtFooter = TextView(this)
+        txtFooter.text = resources.getQuantityString(R.plurals.footer_text, adapter.count, adapter.count)
+        txtFooter.setBackgroundColor(Color.LTGRAY)
+        txtFooter.gravity = Gravity.END
+        txtFooter.setPadding(0, padding, padding, padding)
+        listView.addFooterView(txtFooter)
     }
 }
