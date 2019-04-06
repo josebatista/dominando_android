8b7886079fe999f5a4d0dbb8b0615db2c631a6e3
Jose Pereira <josebatistapereira@gmail.com>
2019-04-05 22:11:55 -0300

    Testando lista e adapter personalizado

--- cap05/Adapter/app/src/main/java/dominando/android/adapter/MainActivity.kt
+++ cap05/Adapter/app/src/main/java/dominando/android/adapter/MainActivity.kt
@@ -1,12 +1,25 @@
 package dominando.android.adapter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    //0=VW;1=GM;2=Fiat;3=Ford
+    private val vehicles = mutableListOf(
+        Vehicle("Onix", 2018, 1, true, true),
+        Vehicle("Uno", 2007, 2, true, false),
+        Vehicle("Del Rey", 1988, 3, false, true),
+        Vehicle("Gol", 2014, 0, true, true)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val listView = ListView(this)
+        setContentView(listView)
+        val adapter = VehicleAdapter(this, vehicles)
+        listView.adapter = adapter
     }
 }
