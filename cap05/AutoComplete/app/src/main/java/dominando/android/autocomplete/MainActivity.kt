6ee143f7ce4c1c9f26e022caeb330c028959ddbf
Jose Pereira <josebatistapereira@gmail.com>
2019-04-06 21:35:01 -0300

    Incluido AutoCompleteTextView, adicionado adapter com lista para testes

--- cap05/AutoComplete/app/src/main/java/dominando/android/autocomplete/MainActivity.kt
+++ cap05/AutoComplete/app/src/main/java/dominando/android/autocomplete/MainActivity.kt
@@ -1,12 +1,26 @@
 package dominando.android.autocomplete
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cities = listOf(
+            "Caruaru",
+            "Cabo de Santo Agostinho",
+            "Recife",
+            "São Paulo",
+            "Santos",
+            "Santa Cruz"
+        )
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, cities)
+
+        actCities.setAdapter(adapter)
     }
 }
