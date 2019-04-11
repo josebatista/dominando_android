b29105ab46a09666090013b318fa86681bdde858
Jose Pereira <josebatistapereira@gmail.com>
2019-04-10 22:13:41 -0300

    Navegando utilizando web view, e abrindo link na propria webview

--- cap06/WebView/app/src/main/java/dominando/android/webview/MainActivity.kt
+++ cap06/WebView/app/src/main/java/dominando/android/webview/MainActivity.kt
@@ -1,12 +1,49 @@
 package dominando.android.webview
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.TargetApi
+import android.os.Build
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView.loadUrl("http://www.nglauber.com.br")
+        webView.webViewClient = object : WebViewClient() {
+
+            //metodos para evitar que ao clicar em um link ele abra o navegador
+
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                view.loadUrl(url)
+                return false //retornando false ira abrir na propria web view
+            }
+
+            @TargetApi(Build.VERSION_CODES.N)
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+                view.loadUrl(request.url.toString())
+                return false //retornando false ira abrir na propria web view
+            }
+
+            /*
+
+            Metodos que determinando quando uma pagina comeca a carregar e quando o carregamento foi concluido
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+            }
+            */
+        }
+
     }
 }
