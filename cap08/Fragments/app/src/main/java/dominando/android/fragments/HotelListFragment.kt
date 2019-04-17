ffaaf1bf441b54d7efe83e5d28529735fe38fa2a
Jose Pereira <josebatistapereira@gmail.com>
2019-04-16 23:02:56 -0300

    Fragment criado, listando os hoteis e adicionado a activity principal

---
+++ cap08/Fragments/app/src/main/java/dominando/android/fragments/HotelListFragment.kt
@@ -0,0 +1,24 @@
+package dominando.android.fragments
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.fragment.app.ListFragment
+
+class HotelListFragment : ListFragment(), HotelListView {
+
+    private val presenter = HotelListPresenter(this, MemoryRepository)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        presenter.searchHotels("")
+    }
+
+    override fun showHotels(hotels: List<Hotel>) {
+        val adapter = ArrayAdapter<Hotel>(requireContext(), android.R.layout.simple_list_item_1, hotels)
+        listAdapter = adapter
+    }
+
+    override fun showHotelDetail(hotel: Hotel) {
+        //Sera implementado em breve...
+    }
+}
