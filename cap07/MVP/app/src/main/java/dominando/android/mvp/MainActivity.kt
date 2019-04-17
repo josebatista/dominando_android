0335820871f63a222079dd655dbed7580b45f673
Jose Pereira <josebatistapereira@gmail.com>
2019-04-16 21:33:38 -0300

    Implementado View para que realize a consulta utilizando o presenter deixando a view sem logica de negocio.

--- cap07/MVP/app/src/main/java/dominando/android/mvp/MainActivity.kt
+++ cap07/MVP/app/src/main/java/dominando/android/mvp/MainActivity.kt
@@ -2,11 +2,34 @@ package dominando.android.mvp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), InternetSearchView {
+
+    val presenter = InternetSearchPresenter(this, GoogleInternetSearch())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonSearch.setOnClickListener {
+            presenter.search(editText.text.toString())
+        }
+    }
+
+    override fun showProgress() {
+        //EXIBIR MENSAGEM DE PROGRESSO (EX.: PROCURANDO...)
+    }
+
+    override fun hideProgress() {
+        //OCULTAR PROGRESSO
+    }
+
+    override fun showResults(results: List<SearchResult>) {
+        //EXIBIR O RESULTADO (EM UMA RECYCLERVIEW, POR EXEMPLO)
+    }
+
+    override fun showSearchError() {
+        //EXIBIR MENSAGEM DE ERRO
     }
 }
