84bd7235837318f72a61b6d5b8cfad321c552934
Jose Pereira <josebatistapereira@gmail.com>
2019-04-25 22:22:12 -0300

    Adicionado acao de compartilhamento

--- cap08/Fragments/app/src/main/java/dominando/android/fragments/HotelDetailsFragment.kt
+++ cap08/Fragments/app/src/main/java/dominando/android/fragments/HotelDetailsFragment.kt
@@ -1,9 +1,10 @@
 package dominando.android.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.ShareActionProvider
+import androidx.core.view.MenuItemCompat
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_hotel_details.*
 
@@ -12,6 +13,13 @@ class HotelDetailsFragment : Fragment(), HotelDetailsView {
     private val presenter = HotelDetailsPresenter(this, MemoryRepository)
     private var hotel: Hotel? = null
 
+    private var shareActionProvider: ShareActionProvider? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_hotel_details, container, false)
     }
@@ -19,6 +27,14 @@ class HotelDetailsFragment : Fragment(), HotelDetailsView {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         presenter.loadHotelDetails(arguments?.getLong(EXTRA_HOTEL_ID, -1) ?: -1)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.hotel_details, menu)
+        val shareItem = menu?.findItem(R.id.action_share)
+        shareActionProvider = MenuItemCompat.getActionProvider(shareItem) as? ShareActionProvider
+        setShareIntent()
     }
 
     override fun showHotelDetails(hotel: Hotel) {
@@ -32,6 +48,15 @@ class HotelDetailsFragment : Fragment(), HotelDetailsView {
         txtName.text = getString(R.string.error_hotel_not_found)
         txtAddress.visibility = View.GONE
         rtbRating.visibility = View.GONE
+    }
+
+    private fun setShareIntent() {
+        val text = getString(R.string.share_text, hotel?.name, hotel?.rating)
+        shareActionProvider?.setShareIntent(Intent(Intent.ACTION_SEND).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT)
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, text)
+        })
     }
 
     companion object {
