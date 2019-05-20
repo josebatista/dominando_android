0d4db5c11190ffae17a2857e1dcf25c78a777e1a
Jose Pereira <josebatistapereira@gmail.com>
2019-05-20 20:44:52 -0300

    App WebService.

---
+++ cap14/WebService/app/src/main/java/dominando/android/hotel/details/HotelDetailsViewModel.kt
@@ -0,0 +1,10 @@
+package dominando.android.hotel.details
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import dominando.android.hotel.model.Hotel
+import dominando.android.hotel.repository.HotelRepository
+
+class HotelDetailsViewModel(private val repository: HotelRepository): ViewModel() {
+    fun loadHotelDetails(id: Long): LiveData<Hotel> = repository.hotelById(id)
+}
---
+++ cap14/WebService/app/src/main/java/dominando/android/hotel/di/AndroidModule.kt
@@ -0,0 +1,26 @@
+package dominando.android.hotel.di
+
+import dominando.android.hotel.details.HotelDetailsViewModel
+import dominando.android.hotel.form.HotelFormViewModel
+import dominando.android.hotel.list.HotelListViewModel
+import dominando.android.hotel.repository.HotelRepository
+import dominando.android.hotel.repository.room.HotelDatabase
+import dominando.android.hotel.repository.room.RoomRepository
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val androidModule = module {
+    single { this }
+    single {
+        RoomRepository(HotelDatabase.getDatabase(context = get())) as HotelRepository
+    }
+    viewModel {
+        HotelListViewModel(repository = get())
+    }
+    viewModel {
+        HotelDetailsViewModel(repository = get())
+    }
+    viewModel {
+        HotelFormViewModel(repository = get())
+    }
+}
