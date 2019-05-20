6a0f272c845f4acdd767c8bdf2dc3859031b44c7
Jose Pereira <josebatistapereira@gmail.com>
2019-05-19 21:27:31 -0300

    Tratando eventos do sistema.

--- cap13/Broadcast/app/src/main/java/dominando/android/broadcast/EventReceiver.kt
+++ cap13/Broadcast/app/src/main/java/dominando/android/broadcast/EventReceiver.kt
@@ -10,5 +10,11 @@ class EventReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         val action = intent?.action
         Toast.makeText(context, "Ação:\n$action", Toast.LENGTH_LONG).show()
+
+        if (Intent.ACTION_BOOT_COMPLETED == action) {
+            val it = Intent(context, MainActivity::class.java)
+            it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context?.startActivity(it)
+        }
     }
 }
