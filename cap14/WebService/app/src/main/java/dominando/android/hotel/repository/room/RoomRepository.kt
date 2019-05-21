9376aafaebd70be74909c72a3e573b860013d065
Jose Pereira <josebatistapereira@gmail.com>
2019-05-20 21:26:54 -0300

    Preparando a aplicacao para a web.

--- cap14/WebService/app/src/main/java/dominando/android/hotel/repository/room/RoomRepository.kt
+++ cap14/WebService/app/src/main/java/dominando/android/hotel/repository/room/RoomRepository.kt
@@ -3,6 +3,7 @@ package dominando.android.hotel.repository.room
 import androidx.lifecycle.LiveData
 import dominando.android.hotel.model.Hotel
 import dominando.android.hotel.repository.HotelRepository
+import dominando.android.hotel.repository.http.Status
 
 class RoomRepository(database: HotelDatabase) : HotelRepository {
 
@@ -10,11 +11,21 @@ class RoomRepository(database: HotelDatabase) : HotelRepository {
 
     override fun save(hotel: Hotel) {
         if (hotel.id == 0L) {
+            hotel.status = Status.INSERT
             val id = hotelDao.insert(hotel)
             hotel.id = id
         } else {
+            hotel.status = Status.UPDATE
             hotelDao.update(hotel)
         }
+    }
+
+    override fun insert(hotel: Hotel): Long {
+        return hotelDao.insert(hotel)
+    }
+
+    override fun update(hotel: Hotel) {
+        hotelDao.update(hotel)
     }
 
     override fun remove(vararg hotels: Hotel) {
@@ -27,5 +38,13 @@ class RoomRepository(database: HotelDatabase) : HotelRepository {
 
     override fun search(term: String): LiveData<List<Hotel>> {
         return hotelDao.search(term)
+    }
+
+    override fun hotelByServerId(serverId: Long): Hotel? {
+        return hotelDao.hotelByServerId(serverId)
+    }
+
+    override fun pending(): List<Hotel> {
+        return hotelDao.pending()
     }
 }
--- cap14/WebService/app/src/main/java/dominando/android/hotel/repository/sqlite/DBConstants.kt
+++ cap14/WebService/app/src/main/java/dominando/android/hotel/repository/sqlite/DBConstants.kt
@@ -7,3 +7,8 @@ const val COLUMN_ID = "_id"
 const val COLUMN_NAME = "name"
 const val COLUMN_ADDRESS = "address"
 const val COLUMN_RATING = "rating"
+
+const val COLUMN_PHOTO_URL = "photoUrl"
+const val COLUMN_USER = "user"
+const val COLUMN_STATUS = "status"
+const val COLUMN_SERVER_ID = "serverId"
