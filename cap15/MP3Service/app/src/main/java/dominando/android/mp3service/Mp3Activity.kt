384e779a69e845084c43650bb01cc030651aa32c
Jose Pereira <josebatistapereira@gmail.com>
2019-05-27 20:09:32 -0300

    App MP3Service

---
+++ cap15/MP3Service/app/src/main/java/dominando/android/mp3service/Mp3Activity.kt
@@ -0,0 +1,12 @@
+package dominando.android.mp3service
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Mp3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mp3)
+    }
+}
