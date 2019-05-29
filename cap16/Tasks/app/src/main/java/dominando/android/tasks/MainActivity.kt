1dce34f5568db1ebbbb64f9bf243e625fc5e02a3
Jose Pereira <josebatistapereira@gmail.com>
2019-05-28 21:52:01 -0300

    Alterado codigo para agendar tarefa.

--- cap16/Tasks/app/src/main/java/dominando/android/tasks/MainActivity.kt
+++ cap16/Tasks/app/src/main/java/dominando/android/tasks/MainActivity.kt
@@ -2,24 +2,28 @@ package dominando.android.tasks
 
 import android.os.Bundle
 import android.os.Handler
-import android.os.Message
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.ref.WeakReference
 
 class MainActivity : AppCompatActivity() {
 
-    private val handler: MyHandler by lazy { MyHandler(this) }
-    private var thread: MyThread? = null
+//    private val handler: MyHandler by lazy { MyHandler(this) }
+//    private var thread: MyThread? = null
+
+    private lateinit var handler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        handler = Handler()
+
         btnStart.setOnClickListener {
-            thread = MyThread(handler)
-            thread?.start()
+            //            thread = MyThread(handler)
+//            thread?.start()
             btnStart.isEnabled = false
+            handler.post(MyThread(this))
         }
 
     }
@@ -27,48 +31,70 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         handler.removeCallbacksAndMessages(null)
-        thread?.interrupt()
+//        thread?.interrupt()
     }
 
-    private class MyThread(handler: Handler) : Thread() {
+    private class MyThread(activity: MainActivity) : Thread() {
 
-        private val handlerRef = WeakReference<Handler>(handler)
+        private val activityRef = WeakReference<MainActivity>(activity)
+        private var count = 0
 
         override fun run() {
             super.run()
-            handlerRef.get()?.let { handler ->
-                for (i in 0 until 10) {
-                    val message = Message()
-                    message.what = MESSAGE_COUNT
-                    message.arg1 = i
-                    handler.sendMessage(message)
-                    try {
-                        sleep(1000)
-                    } catch (e: InterruptedException) {
-                        e.printStackTrace()
-                    }
-                }
-                handler.sendEmptyMessage(MESSAGE_FINISH)
-            }
-        }
-    }
-
-    private class MyHandler(activity: MainActivity) : Handler() {
-
-        val activityRef = WeakReference<MainActivity>(activity)
-
-        override fun handleMessage(msg: Message) {
-            super.handleMessage(msg)
             activityRef.get()?.let { activity ->
-                if (msg.what == MESSAGE_COUNT) {
-                    activity.txtMessage.text = "Contador: ${msg.arg1}"
-                } else if (msg.what == MESSAGE_FINISH) {
+                if (count < 10) {
+                    count++
+                    activity.txtMessage.text = "Contador: $count"
+                    activity.handler.postDelayed(this, 1000)
+                } else {
+                    count = 0
                     activity.txtMessage.text = "Acabou!"
                     activity.btnStart.isEnabled = true
                 }
             }
         }
+
     }
+
+//    private class MyThread(handler: Handler) : Thread() {
+//
+//        private val handlerRef = WeakReference<Handler>(handler)
+//
+//        override fun run() {
+//            super.run()
+//            handlerRef.get()?.let { handler ->
+//                for (i in 0 until 10) {
+//                    val message = Message()
+//                    message.what = MESSAGE_COUNT
+//                    message.arg1 = i
+//                    handler.sendMessage(message)
+//                    try {
+//                        sleep(1000)
+//                    } catch (e: InterruptedException) {
+//                        e.printStackTrace()
+//                    }
+//                }
+//                handler.sendEmptyMessage(MESSAGE_FINISH)
+//            }
+//        }
+//    }
+
+//    private class MyHandler(activity: MainActivity) : Handler() {
+//
+//        val activityRef = WeakReference<MainActivity>(activity)
+//
+//        override fun handleMessage(msg: Message) {
+//            super.handleMessage(msg)
+//            activityRef.get()?.let { activity ->
+//                if (msg.what == MESSAGE_COUNT) {
+//                    activity.txtMessage.text = "Contador: ${msg.arg1}"
+//                } else if (msg.what == MESSAGE_FINISH) {
+//                    activity.txtMessage.text = "Acabou!"
+//                    activity.btnStart.isEnabled = true
+//                }
+//            }
+//        }
+//    }
 
     companion object {
         private const val MESSAGE_COUNT = 1
