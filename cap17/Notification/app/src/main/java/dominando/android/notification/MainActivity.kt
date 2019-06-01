00c88bb66835280dc388c104cb01fe8657b8ffb2
Jose Pereira <josebatistapereira@gmail.com>
2019-05-31 23:39:55 -0300

    Modificado layout e implementada logica dos botoes. Adicionado dois icones que serao utilizados. Criado objeto que sera responsavel por criar as notificacoes para os testes.

--- cap17/Notification/app/src/main/java/dominando/android/notification/MainActivity.kt
+++ cap17/Notification/app/src/main/java/dominando/android/notification/MainActivity.kt
@@ -1,12 +1,22 @@
 package dominando.android.notification
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSimple.setOnClickListener { NotificationUtils.notificationSimples(this) }
+        btnTapAction.setOnClickListener { NotificationUtils.notificationWithTapAction(this) }
+        btnBigText.setOnClickListener { NotificationUtils.notificationBigText(this) }
+        btnActionButton.setOnClickListener { NotificationUtils.notificationWithButtonAction(this) }
+        btnDirectReply.setOnClickListener { NotificationUtils.notificationAutoReply(this) }
+        btnInbox.setOnClickListener { NotificationUtils.notificationInbox(this) }
+        btnHeadsUp.setOnClickListener { NotificationUtils.notificationHeadsUp(this) }
+
     }
 }
---
+++ cap17/Notification/app/src/main/java/dominando/android/notification/NotificationUtils.kt
@@ -0,0 +1,15 @@
+package dominando.android.notification
+
+import android.content.Context
+
+object NotificationUtils {
+
+    fun notificationSimples(context: Context) {}
+    fun notificationWithTapAction(context: Context) {}
+    fun notificationBigText(context: Context) {}
+    fun notificationWithButtonAction(context: Context) {}
+    fun notificationAutoReply(context: Context) {}
+    fun notificationInbox(context: Context) {}
+    fun notificationHeadsUp(context: Context) {}
+
+}
