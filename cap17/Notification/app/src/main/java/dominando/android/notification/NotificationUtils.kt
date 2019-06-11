42791b14ffced4ea302697e9ea42f444e898e840
Jose Pereira <josebatistapereira@gmail.com>
2019-06-10 21:37:42 -0300

    Acao de clicar na notificacao.

--- cap17/Notification/app/src/main/java/dominando/android/notification/NotificationUtils.kt
+++ cap17/Notification/app/src/main/java/dominando/android/notification/NotificationUtils.kt
@@ -2,13 +2,16 @@ package dominando.android.notification
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.TaskStackBuilder
 
 object NotificationUtils {
 
@@ -49,7 +52,42 @@ object NotificationUtils {
         notificationManager.notify(1, notificationBuilder.build())
     }
 
-    fun notificationWithTapAction(context: Context) {}
+//    private fun getContentIntent(context: Context): PendingIntent? {
+//        val intent = Intent(context, MainActivity::class.java).apply {
+//            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//        }
+//        return PendingIntent.getActivity(context, 0, intent, 0)
+//    }
+
+    private fun getContentIntent(context: Context): PendingIntent? {
+        val detailsIntent = Intent(context, DetailActivity::class.java).apply {
+            putExtra(DetailActivity.EXTRA_MESSAGE, "Via notificação")
+        }
+
+        return TaskStackBuilder.create(context)
+            .addNextIntentWithParentStack(detailsIntent)
+            .getPendingIntent(1, PendingIntent.FLAG_UPDATE_CURRENT)
+
+    }
+
+    fun notificationWithTapAction(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(context)
+        }
+        val notificationBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_favorite)
+            .setContentTitle(context.getString(R.string.notif_title))
+            .setContentText(context.getString(R.string.notif_text))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setColor(ActivityCompat.getColor(context, R.color.colorAccent))
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setContentIntent(getContentIntent(context))
+            .setAutoCancel(true)
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.notify(2, notificationBuilder.build())
+    }
+
     fun notificationBigText(context: Context) {}
     fun notificationWithButtonAction(context: Context) {}
     fun notificationAutoReply(context: Context) {}
