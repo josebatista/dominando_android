64f529b81e5fa1dbef2ea6acd52ee14e2b4b1647
Jose Pereira <josebatistapereira@gmail.com>
2019-06-27 22:42:17 -0300

    App livros.

---
+++ cap20/Livros/app/src/main/java/dominando/android/livros/BookListActivity.kt
@@ -0,0 +1,12 @@
+package dominando.android.livros
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BookListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_list)
+    }
+}
