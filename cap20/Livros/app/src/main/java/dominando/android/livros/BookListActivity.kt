21866447591cd6c9560811f6965210aba6a28bbe
Jose Pereira <josebatistapereira@gmail.com>
2019-07-01 22:12:46 -0300

    Adicionado activity utilizando databinding. BookListActivity alterada para verificar funcionamento do que foi feito.

--- cap20/Livros/app/src/main/java/dominando/android/livros/BookListActivity.kt
+++ cap20/Livros/app/src/main/java/dominando/android/livros/BookListActivity.kt
@@ -2,11 +2,31 @@ package dominando.android.livros
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dominando.android.livros.model.Book
+import dominando.android.livros.model.MediaType
+import dominando.android.livros.model.Publisher
 
 class BookListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_list)
+
+        BookDetailsActivity.start(
+            this,
+            Book(
+                id = "1",
+                title = "Dominando o Android com Kotlin",
+                author = "Nelson Glauber",
+                coverUrl = "https://s3.novatec.com.br/capas-ampliadas/capa-ampliada-9788575224632.jpg",
+                pages = 954,
+                year = 2018,
+                publisher = Publisher("1", "Novatec"),
+                available = true,
+                mediaType = MediaType.PAPER,
+                rating = 5.0f
+            )
+        )
+
     }
 }
