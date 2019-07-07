1fecb0c75dfcb5ea71ca605a01360777765fc10a
Jose Pereira <josebatistapereira@gmail.com>
2019-07-06 21:44:31 -0300

    App Livros - Firebase.

---
+++ cap21/Livros/app/src/main/java/dominando/android/livros/BookAdapter.kt
@@ -0,0 +1,36 @@
+package dominando.android.livros
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import dominando.android.livros.databinding.ItemBookBinding
+import dominando.android.livros.model.Book
+
+class BookAdapter(val books: List<Book>, private val onClick: (Book) -> Unit) :
+    RecyclerView.Adapter<BookAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_book, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = books.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding?.run {
+            val currentBook = books[position]
+            book = currentBook
+            root.setOnClickListener {
+                onClick(currentBook)
+            }
+            executePendingBindings()
+        }
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val binding = DataBindingUtil.bind<ItemBookBinding>(view)
+    }
+
+}
