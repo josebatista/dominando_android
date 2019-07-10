7a3ecbd3b04be558f3d15a08070e8a0d2843f1eb
Jose Pereira <josebatistapereira@gmail.com>
2019-07-09 22:34:34 -0300

    .

--- cap21/Livros/app/src/main/java/dominando/android/livros/livedata/LiveDataExtensions.kt
+++ cap21/Livros/app/src/main/java/dominando/android/livros/livedata/LiveDataExtensions.kt
@@ -3,6 +3,7 @@ package dominando.android.livros.livedata
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 
+@Throws(Throwable::class)
 fun <T> LiveData<T>.observeOnce(observer: Observer<T>) {
     observeForever(object : Observer<T> {
         override fun onChanged(t: T) {
