45604b2d8099c9f738f442dd783c9d8c6364d0bf
José Batista de A. Pereira <josebatistapereira@gmail.com>
2019-07-27 22:09:33 -0300

    Criado fragment com lista dos contatos. Chamando fragment a partir da activity. Mostrando detalhes ao clicar em um contato especifico.

--- cap22/Contatos/app/src/main/java/dominando/android/contatos/ContactsActivity.kt
+++ cap22/Contatos/app/src/main/java/dominando/android/contatos/ContactsActivity.kt
@@ -1,7 +1,11 @@
 package dominando.android.contatos
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 class ContactsActivity : AppCompatActivity() {
 
@@ -9,4 +13,48 @@ class ContactsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
     }
+
+    override fun onResume() {
+        super.onResume()
+        if (!hasPermission(Manifest.permission.READ_CONTACTS) || !hasPermission(Manifest.permission.WRITE_CONTACTS)) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    Manifest.permission.READ_CONTACTS,
+                    Manifest.permission.WRITE_CONTACTS
+                ), RC_PERMISSION_CONTACTS
+            )
+        } else {
+            init()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == RC_PERMISSION_CONTACTS && grantResults.isNotEmpty()) {
+            if (!grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                Toast.makeText(this, R.string.error_permission, Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+
+    private fun hasPermission(permission: String): Boolean {
+        return ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun init() {
+        if (supportFragmentManager.findFragmentByTag(TAG_CONTACT_LIST) == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, ContactListFragment(), TAG_CONTACT_LIST)
+                .commit()
+        }
+    }
+
+
+    companion object {
+        private const val TAG_CONTACT_LIST = "contacts_fragment"
+        private const val RC_PERMISSION_CONTACTS = 1
+    }
+
 }
