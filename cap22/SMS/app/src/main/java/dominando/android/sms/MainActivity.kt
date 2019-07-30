67f5ad8e2be4f634dc5e6170a2d55965266df6f4
José Pereira <josebatistapereira@gmail.com>
2019-07-29 21:45:06 -0300

    Criado receiver para identificar recebimento do SMS.

--- cap22/SMS/app/src/main/java/dominando/android/sms/MainActivity.kt
+++ cap22/SMS/app/src/main/java/dominando/android/sms/MainActivity.kt
@@ -1,12 +1,29 @@
 package dominando.android.sms
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val permissions = arrayOf(
+            Manifest.permission.READ_SMS,
+            Manifest.permission.RECEIVE_SMS
+        )
+
+        val hasPermission = permissions.all {
+            ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+        }
+
+        if (!hasPermission) {
+            ActivityCompat.requestPermissions(this, permissions, 0)
+        }
     }
 }
