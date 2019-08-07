d23bc36f31d6f20f1bc74aebec5934fbcb14a790
José Pereira <josebatistapereira@gmail.com>
2019-08-06 21:41:51 -0300

    Criando uma Activity de camera (inicio).

---
+++ cap25/Multimidia/app/src/main/java/dominando/android/multimidia/DominandoCameraActivity.kt
@@ -0,0 +1,12 @@
+package dominando.android.multimidia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DominandoCameraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dominando_camera)
+    }
+}
