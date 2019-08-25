f09e95b2227de1674b02eb8d6edc903ef97ce32e
José Pereira <josebatistapereira@gmail.com>
2019-08-24 23:29:12 -0300

    Animators e Interpolators.

--- cap26/Animacoes/app/src/main/java/dominando/android/animacoes/MainActivity.kt
+++ cap26/Animacoes/app/src/main/java/dominando/android/animacoes/MainActivity.kt
@@ -1,12 +1,15 @@
 package dominando.android.animacoes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, ViewAnimationsActivity::class.java))
     }
 }
