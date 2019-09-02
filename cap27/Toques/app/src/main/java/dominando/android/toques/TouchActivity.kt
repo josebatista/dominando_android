45691f209c7528bbeb273548032d1919ba6cba17
José Pereira <josebatistapereira@gmail.com>
2019-09-02 20:11:22 -0300

    App Toques.

---
+++ cap27/Toques/app/src/main/java/dominando/android/toques/TouchActivity.kt
@@ -0,0 +1,12 @@
+package dominando.android.toques
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TouchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_touch)
+    }
+}
