03ff9d9990de66784c087de8d9969357a4f401ab
José Pereira <josebatistapereira@gmail.com>
2019-09-23 21:46:15 -0300

    Lendo informacoes da web com GSON e OkHttp.

---
+++ cap30/EngHaw/app/src/main/java/dominando/android/enghaw/model/Album.kt
@@ -0,0 +1,29 @@
+package dominando.android.enghaw.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class Album(
+    @SerializedName("titulo")
+    val title: String,
+
+    @SerializedName("capa")
+    val cover: String,
+
+    @SerializedName("capa_big")
+    val coveBigr: String,
+
+    @SerializedName("ano")
+    val year: Int,
+
+    @SerializedName("gravadora")
+    val recordingCompany: String,
+
+    @SerializedName("formacao")
+    val formation: List<String>,
+
+    @SerializedName("faixas")
+    val tracks: List<String>
+) : Parcelable
