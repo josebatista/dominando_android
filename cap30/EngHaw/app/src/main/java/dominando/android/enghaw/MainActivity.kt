a18a0b82ad3ad24b3620ae8ddc69a71d525ee444
José Pereira <josebatistapereira@gmail.com>
2019-09-24 21:00:20 -0300

    Adicionado Toolbar e fragment na MainActivity para carregar a lista.

--- cap30/EngHaw/app/src/main/java/dominando/android/enghaw/MainActivity.kt
+++ cap30/EngHaw/app/src/main/java/dominando/android/enghaw/MainActivity.kt
@@ -1,12 +1,16 @@
 package dominando.android.enghaw
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(Color.WHITE)
     }
 }
