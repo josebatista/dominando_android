03c71e30766b7d5a2e2bca8333f1e143f6d498a2
José Pereira <josebatistapereira@gmail.com>
2019-09-30 22:31:17 -0300

    Persistindo os albuns favoritos.

--- cap30/EngHaw/app/src/main/java/dominando/android/enghaw/model/Album.kt
+++ cap30/EngHaw/app/src/main/java/dominando/android/enghaw/model/Album.kt
@@ -1,11 +1,18 @@
 package dominando.android.enghaw.model
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
+@Entity
 class Album(
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0L,
+
     @SerializedName("titulo")
     val title: String,
 
